27f69798f76ef8c4064b09d9c39a39f03ac26bac
craig91 <craigdejean1@gmail.com>
2025-04-13 16:48:36 +0900

    added more example code

--- structptr.cpp
+++ structptr.cpp
@@ -9,5 +9,10 @@ int main() {
         dog.type = "Golden Retriever";
 
         Animal* ptr = &dog;
-        std::cout << ptr->type << "\n";
+        
+        std::cout << "Value of dog.type: " << dog.type << "\n";
+        std::cout << "Value via the pointer: " << ptr->type << "\n";
+        std::cout << "Address of the dog: " << &dog << "\n"; 
+        std::cout << "Value stored in the pointer: " << ptr << "\n";
+        std::cout << "Address of the ptr itself: " << &ptr << "\n";
 }
